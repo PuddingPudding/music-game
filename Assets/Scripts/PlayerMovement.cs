87b0d6e903ec3cb5e534f271a56a465ac5e47c04
PuddingPudding <hwsh70119@gmail.com>
2018-01-17 16:33:58 +0800

    先做出簡單的左右移動，下次commit可能要很久以後了...

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    public float speed = 3f;
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float finalSpeedX = 0;
+
+        if(Input.GetKey(KeyCode.LeftArrow) )
+        {
+            finalSpeedX += -1 * speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            finalSpeedX += 1 * speed;
+        }
+
+        this.rigidbody.velocity = new Vector2(finalSpeedX , this.rigidbody.velocity.y);
+    }
+}
